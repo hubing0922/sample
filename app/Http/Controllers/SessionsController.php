2fd9083b4fbea5962f2124c50984fefd3a93e0a4
bing <471083525@qq.com>
2018-10-30 20:10:06 +0000

    master

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,7 +9,7 @@ class SessionsController extends Controller
 {
    public function create()
     {
-        return view('sessions.create');
+        return view('Sessions.create');
     }
 
      public function store(Request $request)
