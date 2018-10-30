7a7ca55becd86121a377fb51ed9f252e41961cd2
bing <471083525@qq.com>
2018-10-30 15:16:06 +0000

    Updated database configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__.'/helpers.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -51,5 +52,4 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-
 return $app;
